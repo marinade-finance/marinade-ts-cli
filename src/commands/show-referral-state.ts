7f65af8c95cd82d98dbb7c9ad2aea4b9ae37dd27
luciotato <luciotato@gmail.com>
2021-12-20 10:00:43 -0300

    small changes in output

--- src/commands/show-referral-state.ts
+++ src/commands/show-referral-state.ts
@@ -17,24 +17,36 @@ export async function showReferralStateAction (referral: string): Promise<void> 
   console.log('---')
   console.log('Partner Account:', state.partnerAccount.toBase58())
   console.log('Token Partner Account:', state.tokenPartnerAccount.toBase58())
-  console.log('Paused:', state.pause)
+  if (state.pause) console.log('--PAUSED--');
 
-  console.log('---')
   const lastTransferTimeTimestamp = state.lastTransferTime.toNumber()
   const lastTransferTime = new Date(lastTransferTimeTimestamp * 1e3)
-  console.log('Transfer duration:', formatDuration(state.transferDuration), `(${state.transferDuration})`)
-  console.log('Last Transfer Time:', lastTransferTime, `(${lastTransferTimeTimestamp})`)
+  // commented because auto-transfers are not enabled yet
+  // console.log('---')
+  // console.log('Transfer duration:', formatDuration(state.transferDuration), `(${state.transferDuration})`)
+  // console.log('Last Transfer Time:', lastTransferTime, `(${lastTransferTimeTimestamp})`)
+  // console.log('Max Net Stake:', MarinadeUtils.lamportsToSol(state.maxNetStake))
+  // console.log('Base fee:', state.baseFee, 'Max fee:', state.maxFee)
+  // console.log('---')
 
-  console.log('---')
-  console.log('Deposit SOL amount:', MarinadeUtils.lamportsToSol(state.depositSolAmount), 'operations:', state.depositSolOperations.toNumber())
-  console.log('Deposit Stake Account amount:', MarinadeUtils.lamportsToSol(state.depositStakeAccountAmount), 'operations:', state.depositStakeAccountOperations.toNumber())
-  console.log('Liquid Unstake SOL amount:', MarinadeUtils.lamportsToSol(state.liqUnstakeSolAmount), 'mSOL amount:', MarinadeUtils.lamportsToSol(state.liqUnstakeMsolAmount), 'operations:', state.liqUnstakeOperations.toNumber())
-  console.log('Delayed Unstake amount:', MarinadeUtils.lamportsToSol(state.delayedUnstakeAmount), 'operations:', state.delayedUnstakeOperations.toNumber())
+  console.log()
+  console.log('Deposit SOL');
+  console.log(' -- SOL amount:', MarinadeUtils.lamportsToSol(state.depositSolAmount));
+  console.log(' -- operations:', state.depositSolOperations.toNumber())
+  console.log()
+  console.log('Deposit Stake Account');
+  console.log(' -- SOL amount:', MarinadeUtils.lamportsToSol(state.depositStakeAccountAmount));
+  console.log(' -- operations:', state.depositStakeAccountOperations.toNumber())
+  console.log()
+  console.log('Liquid Unstake');
+  console.log(' -- SOL amount:', MarinadeUtils.lamportsToSol(state.liqUnstakeSolAmount));
+  console.log(' --mSOL amount:', MarinadeUtils.lamportsToSol(state.liqUnstakeMsolAmount))
+  console.log(' --mSOL fees  :', MarinadeUtils.lamportsToSol(state.liqUnstakeMsolFees))
+  console.log(' -- operations:', state.liqUnstakeOperations.toNumber())
+  // commented because Delayed Unstake is not enabled yet
+  // console.log('Delayed Unstake');
+  // console.log(' -- SOL amount:', MarinadeUtils.lamportsToSol(state.delayedUnstakeAmount))
+  // console.log(' -- operations:', state.delayedUnstakeOperations.toNumber())
 
-  console.log('---')
-  console.log('Liquid Unstake mSOL fees:', MarinadeUtils.lamportsToSol(state.liqUnstakeMsolFees))
-  console.log('Max Net Stake:', MarinadeUtils.lamportsToSol(state.maxNetStake))
 
-  console.log('---')
-  console.log('Base fee:', state.baseFee, 'Max fee:', state.maxFee)
 }
