822d6db38f91f8be15ac3d1e800d8565a9ac5984
Ondra Chaloupka <chalda@marinade.finance>
2023-08-10 08:50:10 +0200

    common-cli: fix for temp file creation to use path for building path

--- packages/cli-common/src/createTempFileKeypair.ts
+++ packages/cli-common/src/createTempFileKeypair.ts
@@ -2,6 +2,7 @@ import { Keypair } from '@solana/web3.js'
 import { writeFileSync } from 'fs'
 import { mkdtemp, rm } from 'fs/promises'
 import { tmpdir } from 'os'
+import * as path from 'path'
 
 export async function createTempFileKeypair(seed?: Keypair): Promise<{
   path: string
@@ -10,11 +11,12 @@ export async function createTempFileKeypair(seed?: Keypair): Promise<{
 }> {
   const keypair = seed ?? new Keypair()
 
-  const folder = await mkdtemp(tmpdir() + '/marinade-cli-')
-  const path = `${folder}/keypair.json`
-  writeFileSync(path, JSON.stringify(Array.from(keypair.secretKey)))
+  const folderPath = path.join(tmpdir(), 'marinade-cli-')
+  const folder = await mkdtemp(folderPath)
+  const keypairPath = path.join(folder, 'keypair.json')
+  writeFileSync(keypairPath, JSON.stringify(Array.from(keypair.secretKey)))
   const cleanup = async () => {
     await rm(folder, { recursive: true, force: true })
   }
-  return { path, cleanup, keypair }
+  return { path: keypairPath, cleanup, keypair }
 }
