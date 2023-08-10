beb38a5a683462f77cc0ee062d32b23ec8f5e968
Ondra Chaloupka <chalda@marinade.finance>
2023-08-10 16:09:24 +0200

    cli-common: configure pino logger as utility function

---
+++ packages/cli-common/src/pinoLogging.ts
@@ -0,0 +1,21 @@
+import pino, { Logger } from 'pino'
+
+export function configureLogger(level = 'info'): Logger {
+  const pinoAdditionalOptions = process.env.NODE_ENV?.startsWith('prod')
+    ? {
+        singleLine: true,
+        errorLikeObjectKeys: [],
+      }
+    : {}
+  const logger: Logger = pino({
+    transport: {
+      target: 'pino-pretty',
+      options: {
+        translateTime: 'UTC:yyyy-mm-dd HH:MM:ss.l o',
+        ...pinoAdditionalOptions,
+      },
+    },
+    level: level,
+  })
+  return logger
+}
