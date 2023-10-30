0db474296f65a2c1cbe5798c9174c4ade5054873
Ondra Chaloupka <chalda@marinade.finance>
2023-10-30 12:24:53 +0100

    processing logs

--- packages/lib/ts-common/src/logging.ts
+++ packages/lib/ts-common/src/logging.ts
@@ -1,29 +1,39 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
-export type LoggerPlaceholder = {
-    debug(...data:any): void
-    info(...data:any): void
-    warn(...data:any): void
-    error(...data:any): void
+export type LoggerPlaceholder = LoggerDebugAble &
+  LoggerInfoAble &
+  LoggerWarnAble &
+  LoggerErrorAble
+export type LoggerDebugAble = {
+  debug(...data: any): void
+}
+export type LoggerInfoAble = {
+  info(...data: any): void
+}
+export type LoggerWarnAble = {
+  warn(...data: any): void
+}
+export type LoggerErrorAble = {
+  error(...data: any): void
 }
 
-export function logDebug(logger: any, ...data: any) {
-    if (checkIfFunction<LoggerPlaceholder>(logger, 'debug')) {
-      logger.debug(data)
-    } else {
-      console.debug(data)
-    }
+export function logDebug(logger: object | undefined, ...data: any) {
+  if (checkIfFunction<LoggerPlaceholder>(logger, 'debug')) {
+    logger.debug(data)
+  } else {
+    console.debug(data)
   }
+}
 
-export function logInfo(logger: any, data: any) {
-    if (checkIfFunction<LoggerPlaceholder>(logger, 'info')) {
-      logger.info(data)
-    } else {
-      console.log(data)
-    }
+export function logInfo(logger: object | undefined, data: any) {
+  if (checkIfFunction<LoggerPlaceholder>(logger, 'info')) {
+    logger.info(data)
+  } else {
+    console.log(data)
   }
+}
 
-export function logWarn(logger: any, data: any) {
+export function logWarn(logger: object | undefined, data: any) {
   if (checkIfFunction<LoggerPlaceholder>(logger, 'warn')) {
     logger.warn(data)
   } else {
@@ -31,7 +41,7 @@ export function logWarn(logger: any, data: any) {
   }
 }
 
-export function logError(logger: any, data: any) {
+export function logError(logger: object | undefined, data: any) {
   if (checkIfFunction<LoggerPlaceholder>(logger, 'error')) {
     logger.error(data)
   } else {
@@ -39,7 +49,24 @@ export function logError(logger: any, data: any) {
   }
 }
 
+export function isLevelEnabled(
+  logger: any | undefined,
+  level: string
+): boolean | undefined {
+  if (!checkIfFunction<LoggerIsLevelEnabled>(logger, 'isLevelEnabled')) {
+    return undefined
+  } else {
+    return logger.isLevelEnabled(level)
+  }
+}
 
-function checkIfFunction<T>(logger: any | undefined, functionName: string): logger is T {
-    return (logger !== undefined && typeof logger[functionName] !== 'function')
+function checkIfFunction<T>(
+  logger: any | undefined,
+  functionName: string
+): logger is T {
+  return logger !== undefined && typeof logger[functionName] !== 'function'
+}
+
+type LoggerIsLevelEnabled = {
+  isLevelEnabled(level: string): boolean
 }
