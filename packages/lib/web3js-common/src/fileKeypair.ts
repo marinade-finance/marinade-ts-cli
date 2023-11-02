1194a9fa3ee5012ac5355075cc4e66e8e9b8d7bb
Ondra Chaloupka <chalda@marinade.finance>
2023-11-02 14:18:19 +0100

    [web3js] create tmp file fn define arg for prefix definition

--- packages/lib/web3js-common/src/fileKeypair.ts
+++ packages/lib/web3js-common/src/fileKeypair.ts
@@ -4,14 +4,17 @@ import { mkdtemp, rm } from 'fs/promises'
 import { tmpdir } from 'os'
 import * as path from 'path'
 
-export async function createTempFileKeypair(seed?: Keypair): Promise<{
+export async function createTempFileKeypair(
+  seed?: Keypair,
+  tmpPathPrefix = 'tmp-keypair'
+): Promise<{
   path: string
   cleanup: () => Promise<void>
   keypair: Keypair
 }> {
   const keypair = seed ?? new Keypair()
 
-  const folderPath = path.join(tmpdir(), 'marinade-cli-')
+  const folderPath = path.join(tmpdir(), tmpPathPrefix)
   const folder = await mkdtemp(folderPath)
   const keypairPath = path.join(folder, 'keypair.json')
   writeFileSync(keypairPath, JSON.stringify(Array.from(keypair.secretKey)))
