01bf1787572ebf5658d59e5946001bc62e117122
chalda <chalda@marinade.finance>
2026-09-08 09:12:57 +0200

    [version] bump 6.1.0 (#53)

--- packages/marinade-ts-cli/src/index.ts
+++ packages/marinade-ts-cli/src/index.ts
@@ -17,7 +17,7 @@ logger.level = 'debug'
 
 const program = new Command('marinade')
 program
-  .version('6.0.0')
+  .version('6.1.0')
   .allowExcessArguments(false)
   .option(
     '-u, --url <url-or-moniker>',
